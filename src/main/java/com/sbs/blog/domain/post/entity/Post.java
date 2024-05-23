ea995674fa57b7fe58b648c5946fd9bd9e3d9d02
PILSUNyou <dbyo1125@naver.com>
2024-05-23 16:46:29 +0900

    게시글 리스트 ì 데이터 출력

--- src/main/java/com/sbs/blog/domain/post/entity/Post.java
+++ src/main/java/com/sbs/blog/domain/post/entity/Post.java
@@ -2,12 +2,18 @@ package com.sbs.blog.domain.post.entity;
 
 import com.sbs.blog.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Post extends BaseEntity {
 
     private String title;

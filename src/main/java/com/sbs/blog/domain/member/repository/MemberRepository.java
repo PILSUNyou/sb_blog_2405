f91fb2f30fd978061695d20b3145ae30d5564b53
PILSUNyou <dbyo1125@naver.com>
2024-05-23 17:13:27 +0900

    ãMember MVC 세팅, Post author 추가

---
+++ src/main/java/com/sbs/blog/domain/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.sbs.blog.domain.member.repository;
+
+import com.sbs.blog.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
--- src/main/java/com/sbs/blog/domain/post/entity/Post.java
+++ src/main/java/com/sbs/blog/domain/post/entity/Post.java
@@ -1,7 +1,9 @@
 package com.sbs.blog.domain.post.entity;
 
+import com.sbs.blog.domain.member.entity.Member;
 import com.sbs.blog.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,8 +17,8 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @ToString
 public class Post extends BaseEntity {
-
     private String title;
     private String content;
-
+    @ManyToOne
+    private Member author;
 }
